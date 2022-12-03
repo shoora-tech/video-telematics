ffb30e6cbbf7bc4c5528a9e444bf0ef251c5b8fa
shoora-tech <111432753+shoora-tech@users.noreply.github.com>
2022-12-03 12:04:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ function handlerLotin(connection){
     // data = "7e02000038784087664106013c00000000000c000101b02fbb048bd2aa00ee0000000022110614335701040001a33b01040001a33b03020000300199310106250400000000537e";
     try{
         data = data.toString('hex');
+        console.log('Data from device--->',data);
       if(data.slice(0, 2).toLowerCase() == '7e' ){
         if(parseInt(data.slice(2, 4),16) == 2){
            deviceDataObj['identifier'] = data.slice(0, 2);
