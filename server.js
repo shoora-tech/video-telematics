b07a85f61b96d28441522330be0d4960e3d0225a
keshavgangwal <keshav.gangwal@lenskart.in>
2022-12-04 22:06:08 +0530

    Code Push

--- server.js
+++ server.js
@@ -32,6 +32,20 @@ const {Sequelize, DataTypes} = require("sequelize");
      "endpoint": "shoorabackend.caaj1e4fnlaq.ap-south-1.rds.amazonaws.com",
      "port": "5432"
  }
+
+
+const { Client } = require('pg');
+
+const client = new Client({
+    user: DB_DETAILS.username,
+    host: DB_DETAILS.endpoint,
+    database: DB_DETAILS.database,
+    password: DB_DETAILS.password,
+    port: 5432,
+});
+
+client.connect();
+
 function handlerLotin(connection){
   console.log("connection ESTAB");
   tcpServer.getConnections(function (error, count) {
@@ -155,117 +169,138 @@ function readFromSQS(){
 }
 
 async function insertSQSDataInDB(data){
-    const sequelize = new Sequelize(DB_DETAILS.database, DB_DETAILS.username, DB_DETAILS.password, {
-        host: DB_DETAILS.endpoint,
-        dialect: 'postgres'
-    });
-
-    const Device = sequelize.define('alert_realtimedatabase', {
-        // attributes
-        id: {
-            type: Sequelize.UUID,
-            defaultValue: Sequelize.UUIDV4,
-            primaryKey: true,
-        },
-        location_packet_type: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        message_body_length: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        imei: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        message_serial_number: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        alarm_series: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        terminal_status: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        ignition_status: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        latitude: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        longitude: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        height: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        speed: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        directions: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        oraganization: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-
-    });
 
     try {
-        await sequelize.authenticate();
-        await sequelize.sync({alter: true})
 
-        console.log("alert_realtimedatabase",{
-            "uuid" : randomUUID() ,
-            "identifier":data.identifier,
-            "location_packet_type": data.locationPacketType,
-            "message_body_length": data.messageBodyLength,
-            "imei":data.phoneNumber,
-            "message_serial_number":data.msgSerialNumber,
-            "alarm_series":data.alarmSeries,
-            "terminal_status":data.terminalStatus,
-            "ignition_status": data.ignitionStatus,
-            "latitude":data.latitute,
-            "longitude":data.longitute,
-            "height":data.height,
-            "speed":data.speed,
-            "directions":data.direction,
-            "oraganization":"oraganization",
-            "created_at" : new Date(),
-            "updated_at" : new Date() ,
-        })
 
-        const resultData = await Device.create({
-            "uuid" : randomUUID() ,
-            "identifier":data.identifier,
-            "location_packet_type": data.locationPacketType,
-            "message_body_length": data.messageBodyLength,
-            "imei":data.phoneNumber,
-            "message_serial_number":data.msgSerialNumber,
-            "alarm_series":data.alarmSeries,
-            "terminal_status":data.terminalStatus,
-            "ignition_status": data.ignitionStatus,
-            "latitude":data.latitute,
-            "longitude":data.longitute,
-            "height":data.height,
-            "speed":data.speed,
-            "directions":data.direction,
-            "oraganization":"oraganization",
-            "created_at" : new Date(),
-            "updated_at" : new Date() ,
-        });
+
+    const query = `INSERT INTO users (uuid, identifier, location_packet_type, message_body_length,imei,message_serial_number,alarm_series,terminal_status,ignition_status,latitude,longitude,height,speed,directions,oraganization,created_at,updated_at)
+                VALUES (randomUUID(), data.identifier, data.locationPacketType, data.messageBodyLength,data.phoneNumber,data.msgSerialNumber,data.alarmSeries,data.terminalStatus,data.ignitionStatus,data.latitute,data.longitute,data.height,data.speed,data.direction,"oraganization",new Date(),new Date())
+                `;
+
+
+    console.log(query)
+
+    client.query(query, (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('Data insert successful');
         return true;
+
+    });
+    //
+    // const sequelize = new Sequelize(DB_DETAILS.database, DB_DETAILS.username, DB_DETAILS.password, {
+    //     host: DB_DETAILS.endpoint,
+    //     dialect: 'postgres'
+    // });
+    //
+    // // const Device = sequelize.define('alert_realtimedatabase', {
+    // //     // attributes
+    // //     id: {
+    // //         type: Sequelize.UUID,
+    // //         defaultValue: Sequelize.UUIDV4,
+    // //         primaryKey: true,
+    // //     },
+    // //     location_packet_type: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     message_body_length: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     imei: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     message_serial_number: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     alarm_series: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     terminal_status: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     ignition_status: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     latitude: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     longitude: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     height: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     speed: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     directions: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //     oraganization: {
+    // //         type: DataTypes.STRING,
+    // //         allowNull: false
+    // //     },
+    // //
+    // // });
+    //
+    //
+    //     await sequelize.authenticate();
+    //     await sequelize.sync({alter: true})
+    //
+    //     console.log("alert_realtimedatabase",{
+    //         "uuid" : randomUUID() ,
+    //         "identifier":data.identifier,
+    //         "location_packet_type": data.locationPacketType,
+    //         "message_body_length": data.messageBodyLength,
+    //         "imei":data.phoneNumber,
+    //         "message_serial_number":data.msgSerialNumber,
+    //         "alarm_series":data.alarmSeries,
+    //         "terminal_status":data.terminalStatus,
+    //         "ignition_status": data.ignitionStatus,
+    //         "latitude":data.latitute,
+    //         "longitude":data.longitute,
+    //         "height":data.height,
+    //         "speed":data.speed,
+    //         "directions":data.direction,
+    //         "oraganization":"oraganization",
+    //         "created_at" : new Date(),
+    //         "updated_at" : new Date() ,
+    //     })
+    //
+    //     const resultData = await Device.create({
+    //         "uuid" : randomUUID() ,
+    //         "identifier":data.identifier,
+    //         "location_packet_type": data.locationPacketType,
+    //         "message_body_length": data.messageBodyLength,
+    //         "imei":data.phoneNumber,
+    //         "message_serial_number":data.msgSerialNumber,
+    //         "alarm_series":data.alarmSeries,
+    //         "terminal_status":data.terminalStatus,
+    //         "ignition_status": data.ignitionStatus,
+    //         "latitude":data.latitute,
+    //         "longitude":data.longitute,
+    //         "height":data.height,
+    //         "speed":data.speed,
+    //         "directions":data.direction,
+    //         "oraganization":"oraganization",
+    //         "created_at" : new Date(),
+    //         "updated_at" : new Date() ,
+    //     });
 
     } catch (error) {
         console.error('Something went Wrong :', error);
